0d5a7d488fe7da94d85a73f7b1d459b37bb6a9c9
schusterglenn-a11y <schusterglenn@ok.de>
2026-06-15 20:21:57 +0200

    Refactor Cesium viewer initialization and tileset loading

--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
-import { Viewer, Cesium3DTileset, Cesium3DTileStyle } from "cesium";
 import "./style.css";
 
-// Euer neuer Token
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjNmMzN2IwZi1lOTkzLTRiZjgtYmE2Ni0yZGIzNGYyMWQwYWYiLCJpZCI6NDQ0NzMwLCJzdWIiOiJzY2h1c3RlcmdsZW5uIiwiaXNzIjoiaHR0cHM6Ly9hcGkuY2VzaXVtLmNvbSIsImF1ZCI6IlVudGl0bGVkIiwiaWF0IjoxNzgxNTQ1MjA1fQ.ATKApLYOxh_G3ScLkdQ-nWBxX35wnwR-Of2kutOGt68';
 
-const viewer = new Viewer("cesiumContainer", {
-    terrainProvider: null // Flacher Boden, damit nichts versinkt
+const viewer = new Cesium.Viewer("cesiumContainer", {
+    terrain: Cesium.Terrain.fromWorldTerrain(),
 });
 
-// Wir laden das Tileset asynchron, wie von Vite/Cesium gewollt
-Cesium3DTileset.fromIonAssetId(4944770)
-    .then((tileset) => {
+// Die modernisierte und sicherste Schreibweise für die Gebäude:
+Cesium.Cesium3DTileset.fromIonAssetId(4944796)
+    .then(function (tileset) {
         viewer.scene.primitives.add(tileset);
-        viewer.zoomTo(tileset);
+        viewer.zoomTo(tileset); // Nutzt zoomTo statt flyTo, um sicherzugehen
 
-        // Styling
-        tileset.style = new Cesium3DTileStyle({
+        tileset.style = new Cesium.Cesium3DTileStyle({
             color: {
                 conditions: [
                     ["${Height} >= 40", "color('darkred')"],
@@ -29,10 +26,9 @@ Cesium3DTileset.fromIonAssetId(4944770)
                 ]
             }
         });
-        console.log("Tileset erfolgreich geladen!");
     })
-    .catch((error) => {
-        console.error("KRITISCHER FEHLER BEIM LADEN:", error);
+    .catch(function (error) {
+        console.log("Fehler beim Laden:", error);
     });
 
 viewer.shadows = true;
